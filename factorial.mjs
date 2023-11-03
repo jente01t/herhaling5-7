d645be8b819b4098e432812a383ce2c56d8f093c
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-03 21:31:14 +0100

    Update factorial.mjs

--- factorial.mjs
+++ factorial.mjs
@@ -1,17 +1,22 @@
-import * as readline from 'node:readline/promises';
-import{stdin as input, stdout as output} from 'node:process';
-const userInput = readline.createInterface({input, output});
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
 
-let getal = parseFloat(await userInput.question('Geef een getal: '));
+let getal = parseFloat(await userInput.question("Geef een getal: "));
 
-function factorial (getal) {
+if (getal > 0) {
+  function factorial(getal) {
     let uitkomst = 1;
-    for(let i = 1; i <= getal; i++) {
-        uitkomst *= i;
+    for (let i = 1; i <= getal; i++) {
+      uitkomst *= i;
     }
-    return uitkomst
+    return uitkomst;
+  }
+
+  console.log("Factorial van het getal " + getal + " is " + factorial(getal));
+
+  process.exit();
+} else {
+    console.error("Fout");
+    process.exit();
 }
-
-console.log('Factorial van het getal ' + getal + ' is ' + factorial(getal));
-
-process.exit();
