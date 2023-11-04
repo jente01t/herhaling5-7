dc0045c43972db76892e2a979608d50d6d3b7a15
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-04 21:13:19 +0100

    update main

--- zoekenArray.mjs
+++ zoekenArray.mjs
@@ -2,3 +2,21 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
+let namen = ["Amin", "Sofie", "Samantha", "Karel", "Mehdi", "Joris", "Nuria"]
+let zoeknaam = await userInput.question('Geef de naam die je zoekt: ');
+
+function naamZoeken (namen) {
+    let nummer = 0;
+    for (let i = 0; i < namen.length; i++) {
+        if (zoeknaam == namen[i]) {
+            nummer = i + 1;
+        } else {
+            nummer = -1;
+        }
+    }
+    return nummer;
+}
+
+console.log('De naam ' + zoeknaam + ' staat op plaats nummer ' + naamZoeken(namen));
+
+process.exit();
