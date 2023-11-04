befd653f3414d35eb8d382820bb7190e8d7796e9
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-11-04 20:59:37 +0100

    update main

---
+++ gemiddeldeArray.mjs
@@ -0,0 +1,23 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let getallen = [];
+
+for (let i = 0; i < 5; i++) {
+    let getal = parseFloat(await userInput.question('Geef een getal: '));
+    getallen.push(getal);
+}
+
+function gemiddeldeArray (getallen) {
+    let gemiddelde = 0;
+    for (let i = 0; i < getallen.length; i++) {
+        gemiddelde += getallen[i];
+    }
+    gemiddelde /= 5;
+    return gemiddelde;
+}
+
+console.log('Het gemiddelde van de getallen is ' + gemiddeldeArray(getallen));
+
+process.exit();
